d92bf559d9bdc78b97433abe8ec970872ebd9a11
indvd00m <gotoindvdum@gmail.com>
2012-07-16 02:22:14 +0400

    Add Solver project

---
+++ icfp-solver/src/main/java/ru/bosony/solvers/Solver.java
@@ -0,0 +1,38 @@
+package ru.bosony.solvers;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+import ru.bosony.model.mine.Mine;
+
+/**
+ * @author indvdum (gotoindvdum[at]gmail[dot]com) <br>
+ *         16.07.2012 2:16:03
+ * 
+ */
+public class Solver {
+
+	protected Mine				mine;
+	protected BufferedWriter	writer;
+
+	public Solver(Mine mine) {
+		this.mine = mine;
+		writer = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+
+	public void start() {
+
+	}
+
+	public static void main(String[] args) {
+		String map = "";
+		for (String arg : args) {
+			map += arg + "\n";
+		}
+		map = map.replaceAll("\n$", "");
+		Mine mine = new Mine(map);
+		Solver solver = new Solver(mine);
+		solver.start();
+	}
+
+}
